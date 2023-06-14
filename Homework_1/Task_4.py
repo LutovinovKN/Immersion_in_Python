4c253343bc53598a9f99587f64f2fcdc64118644
LutovinovKN <VesperLKN@mail.ru>
2023-06-14 19:19:55 +0300

    4 Задача решена

--- Homework_1/Task_4.py
+++ Homework_1/Task_4.py
@@ -3,5 +3,19 @@
 #Программа должна подсказывать «больше» или «меньше» после каждой попытки.
 # 
 # Для генерации случайного числа используйте код:
-# from random import randint
-# num = randint(LOWER_LIMIT, UPPER_LIMIT)
+import emoji
+from random import randint
+num = randint(0, 1001)
+
+
+n = 0
+while n < 11:
+    attempt = int(input())
+    if attempt == num:
+        print(emoji.emojize('Вы угадали! :thumbs_up:'))
+        break
+    elif attempt < num:
+        print(emoji.emojize('Бери число больше :backhand_index_pointing_up:'))
+    else: print(emoji.emojize("Бери число меньше :backhand_index_pointing_down:"))
+    n += 1
+print(num)
