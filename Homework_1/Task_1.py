7bb8fba998909ea658c6033b53cd092df5842efc
LutovinovKN <VesperLKN@mail.ru>
2023-06-16 12:11:01 +0300

    1 Задача решена

--- Homework_1/Task_1.py
+++ Homework_1/Task_1.py
@@ -1,1 +1,8 @@
 #Решить задачи, которые не успели решить на семинаре.
+table = 'ТАБИЦА УМНОЖЕНИЯ'
+print(table.center(40))
+for num in range(1, 10, 3):
+    for count in range(1, 11):     
+        print   (num, 'x', count, '=', num * count, '\t',
+                (num1 := num + 1), 'x', count, '=', num1 * count,'\t',
+                (num2 := num + 2), 'x', count, '=', num * count) 
