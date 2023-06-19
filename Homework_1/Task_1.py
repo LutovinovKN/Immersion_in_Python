a2d1e6f45f779be6a152bcaa5c5797054daaef79
Konstantin <100907312+LutovinovKN@users.noreply.github.com>
2023-06-19 14:21:37 +0300

    Update Homework_1/Task_1.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- Homework_1/Task_1.py
+++ Homework_1/Task_1.py
@@ -3,6 +3,6 @@ table = 'ТАБИЦА УМНОЖЕНИЯ'
 print(table.center(40))
 for num in range(1, 10, 3):
     for count in range(1, 11):     
-        print   (num, 'x', count, '=', num * count, '\t',
-                (num1 := num + 1), 'x', count, '=', num1 * count,'\t',
-                (num2 := num + 2), 'x', count, '=', num * count) 
+        print(num, 'x', count, '=', num * count, '\t',
+              (num1 := num + 1), 'x', count, '=', num1 * count, '\t',
+              (num2 := num + 2), 'x', count, '=', num * count)
